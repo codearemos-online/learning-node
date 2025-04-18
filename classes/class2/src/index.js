72a38eb6e45c343aa95ce0e7f9c5f8b887d55c2e
codearemos-online <dev@codearemos.online>
2025-04-18 00:06:34 -0500

    feat(callback) Add the use mode for callbacks

--- classes/class2/src/index.js
+++ classes/class2/src/index.js
@@ -1,3 +1,11 @@
-const {emailTemplate} = require("./01-emailTemplate")
-require("./03-depuring")
+const {getUserById} = require("./04-callbacks")
+
+getUserById(4, function(error,user){
+    if(error){
+        return console.log(error)
+    }
+
+    console.log(`User ${user.user.name} ${user.user.lastname}`)
+})
+
 
