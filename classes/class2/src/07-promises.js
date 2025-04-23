8c4c858c14e08341884519035a1561f05c1aae92
codearemos-online <dev@codearemos.online>
2025-04-22 20:58:20 -0500

    fix (promises) Apply promises chaining

--- classes/class2/src/07-promises.js
+++ classes/class2/src/07-promises.js
@@ -1,8 +1,7 @@
 const getPokemonById = (id,callback) => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-   .then(res => res.json().then(data => {
-    callback(data.name)
-   }))
+   .then(res => res.json())
+   .then(data => callback(data.name))
 }
 
 module.exports = getPokemonById
