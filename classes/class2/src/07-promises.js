efb0abdec8d37cc4d1371779d97ea013e99656fd
codearemos-online <dev@codearemos.online>
2025-04-22 21:10:38 -0500

    fix (promises) Removing callbacks and using one then to get the value returned for the fetch

--- classes/class2/src/07-promises.js
+++ classes/class2/src/07-promises.js
@@ -1,7 +1,8 @@
-const getPokemonById = (id,callback) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+const getPokemonById = (id) => {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
    .then(res => res.json())
-   .then(data => callback(data.name))
+   .then(data => data.name)
+   .catch(error => console.log("No se encontro el pokemon"))
 }
 
 module.exports = getPokemonById
--- classes/class2/src/index.js
+++ classes/class2/src/index.js
@@ -1,5 +1,7 @@
 const getPokemonById = require("./07-promises")
 
-getPokemonById(1,(name) => {
+getPokemonById(1).then(name => {
     console.log(name)
 })
+
+
