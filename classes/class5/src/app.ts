93b8de297642401ea408e669311f4489a0baa87c
codearemos-online <dev@codearemos.online>
2025-05-16 21:31:35 -0500

    feat(serverApp) Implement clean arquitecture creating a class and interface

--- classes/class5/src/app.ts
+++ classes/class5/src/app.ts
@@ -1,7 +1,13 @@
 import { yarg } from "../src/pugins/yargs.plugin"
+import { ServerApp } from "../src/presentation/server-app"
 
 const main = async () => {
     console.log(yarg)
+    const {b:base,l:limit} = yarg;
+    console.log(ServerApp.run({
+        base,
+        limit
+    }))
 }
 
 (async () => {
