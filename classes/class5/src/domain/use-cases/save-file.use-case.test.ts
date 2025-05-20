c359008bee9563248cf45e306f7e57c83bfa9d45
codearemos-online <dev@codearemos.online>
2025-05-20 18:53:33 -0500

    feat(spyOn) Use sypeOn for create mocks in testing

--- classes/class5/src/domain/use-cases/save-file.use-case.test.ts
+++ classes/class5/src/domain/use-cases/save-file.use-case.test.ts
@@ -14,4 +14,30 @@ describe('saveDefaultValues', () => {
         const readFile = fs.readFileSync('outputs/table.txt', { encoding: 'utf-8' })
         expect(readFile).toBe('any data')
     })
+
+    it('should return false if folder could not be created',() => {
+        const saveFile = new SaveFile()
+        const mkdirSpy = jest.spyOn(fs,'mkdirSync').mockImplementation(
+            () => {  throw new Error('error')}
+        )
+        const options = {
+            fileContent: 'any data'
+        }
+        const result = saveFile.execute(options)
+        expect(result).toBeFalsy()
+    })
+
+     it('should return false if file could not be created',() => {
+        const saveFile = new SaveFile()
+        const mkdirSpy = jest.spyOn(fs,'writeFileSync').mockImplementation(
+            () => {  throw new Error('error')}
+        )
+        const options = {
+            fileContent: 'any data'
+        }
+        const result = saveFile.execute(options)
+        expect(result).toBeFalsy()
+
+        mkdirSpy.mockRestore()
+    })
 }) 
