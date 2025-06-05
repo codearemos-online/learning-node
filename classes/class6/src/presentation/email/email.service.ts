ce37625e99cf9b16a703b12b0ac6e34f1337d922
codearemos-online <dev@codearemos.online>
2025-06-04 22:31:06 -0500

    feat(sendEmail) Config sendEmail package and make 1 test sending Email

--- classes/class6/src/presentation/email/email.service.ts
+++ classes/class6/src/presentation/email/email.service.ts
@@ -1,3 +1,35 @@
-export class EmailService{
-    
+import nodemailer from 'nodemailer';
+import { envs } from '../../config/plugins/envs.plugins';
+
+export interface EmailOptions {
+    to:string,
+    subject:string,
+    htmlBody:string
 }
+
+export class EmailService{
+    private transporter = nodemailer.createTransport({
+        service:envs.MAIL_SERVICE,
+        auth:{
+            user:envs.MAILER_EMAIL,
+            pass:envs.MAILER_SECRET_KEY
+        }
+    })
+
+    async sendEmail(options:EmailOptions):Promise<boolean>{
+        try{
+            const { to,subject,htmlBody } = options;
+            const sendInfo = await this.transporter.sendMail({
+                to:to,
+                subject:subject,
+                html:htmlBody
+            })
+            console.log('Prueba Diego')
+            console.log(sendInfo)
+            return true;
+        }catch(error){
+            return false;
+        }
+    }
+}
+
--- classes/class6/src/presentation/server.ts
+++ classes/class6/src/presentation/server.ts
@@ -1,25 +1,34 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDataSource } from "../infraestructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infraestructure/repositories/log.repository.impl";
+import { EmailService } from "./email/email.service";
 import { CronService } from "./services/cron-service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
 export class Server {
     public static main() {
         console.log("Server is running");
-        
-        CronService.createJob(
-            '*/2 * * * * *',
-            async () => {
-                const data = new CheckService(
-                    fileSystemLogRepository,
-                    () => console.log(`Server is ok`),
-                    (error) => console.log(error) 
-                );
-                
+       
+        const emailService = new EmailService();
+        emailService.sendEmail({
+            to:'dbeltrandev@gmail.com',
+            subject:'prueba de logs',
+            htmlBody:`<h3>Prueba de email</h3>`
+        });
+
+        //CronService.createJob(
+        //    '*/2 * * * * *',
+        //    async () => {
+        //        const data = new CheckService(
+        //            fileSystemLogRepository,
+        //            () => console.log(`Server is ok`),
+        //            (error) => console.log(error) 
+        //        );
+        //        
                 //console.log(await data.execute('https://google.com'));
-                console.log(await data.execute('http://localhost:3000'));
-            }
-        );
+        //        console.log(await data.execute('http://localhost:3000'));
+        //    }
+        //);*/
+       
     }
 }
