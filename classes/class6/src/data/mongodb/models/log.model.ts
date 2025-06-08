a1ed6c7bcc51c7b7db57fbd7500db611fae0ef9f
codearemos-online <dev@codearemos.online>
2025-06-08 01:18:26 -0500

    feat(mongodb) Create log schema to save data

---
+++ classes/class6/src/data/mongodb/models/log.model.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const logSchema = new mongoose.Schema({
+    level:{
+        type:String,
+        enum:['low','medium','high'],
+        default:'low'
+    },
+    message:{
+        type:String,
+        required:true
+    },
+    origin:String,
+    createdAt:{
+        type:Date,
+        default:new Date()
+    },
+})
+
+export const LogModel = mongoose.model('Log',logSchema) 
