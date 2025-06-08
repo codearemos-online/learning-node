8efeb9973bba1d0878cffbec9c4da4c6ea831ede
codearemos-online <dev@codearemos.online>
2025-06-08 01:47:43 -0500

    feat(mongoDB) Create and save data in mongoDB

--- classes/class6/src/app.ts
+++ classes/class6/src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from "./config/plugins/envs.plugins";
-import { MongoConnection } from "./data/mongodb";
+import { MongoConnection, LogModel } from "./data/mongodb";
 import { Server } from "./presentation/server";
 import 'dotenv/config';
 
@@ -9,10 +9,23 @@ import 'dotenv/config';
 
 async function main() {
     //console.log(envs)
-   
-   MongoConnection.connect({
-    mongoUrl:envs.MONGO_URL,
-    dbName:envs.MONGO_DB_NAME
-   })
+
+    MongoConnection.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME
+    })
+
+    //create a collection = tables, document = registry
+    const newLog = await LogModel.create({
+        message:'Welcome to send data',
+        origin:'app.ts'
+    })
+
+    await newLog.save();
+
+    console.log(newLog)
+
+
+
     //Server.main();    
 }
