5f31e4ce24f5752c0f37e07eea95409c06f04f4a
codearemos-online <dev@codearemos.online>
2025-06-10 18:28:44 -0500

    feat(Mongo) Validating Mongo datasource connection

--- classes/class6/src/presentation/server.ts
+++ classes/class6/src/presentation/server.ts
@@ -2,34 +2,35 @@ import { LogRepository } from "../domain/repository/log.repository";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SendLogEmail } from "../domain/use-cases/email/send-log-email";
 import { FileSystemDataSource } from "../infraestructure/datasources/file-system.datasource";
+import { MongoLogDataSource } from "../infraestructure/datasources/mongo-log.datasource";
 import { LogRepositoryImpl } from "../infraestructure/repositories/log.repository.impl";
 import { EmailService } from "./email/email.service";
 import { CronService } from "./services/cron-service";
 
-const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
+const logRepository = new LogRepositoryImpl(new MongoLogDataSource());
 export class Server {
     public static main() {
         console.log("Server is running");
        
-        const emailService = new EmailService();
-        const sendLogEmail = new SendLogEmail(
+        //const emailService = new EmailService();
+        /*const sendLogEmail = new SendLogEmail(
             fileSystemLogRepository,
             emailService
         ).execute(['dbeltrandev@gmail.com']);
-
-        //CronService.createJob(
-        //    '*/2 * * * * *',
-        //    async () => {
-        //        const data = new CheckService(
-        //            fileSystemLogRepository,
-        //            () => console.log(`Server is ok`),
-        //            (error) => console.log(error) 
-        //        );
-        //        
-                //console.log(await data.execute('https://google.com'));
-        //        console.log(await data.execute('http://localhost:3000'));
-        //    }
-        //);*/
+         */   
+        CronService.createJob(
+            '*/2 * * * * *',
+            async () => {
+                const data = new CheckService(
+                    logRepository,
+                    () => console.log(`Server is ok`),
+                    (error) => console.log(error) 
+                );
+                
+      //console.log(await data.execute('https://google.com'));
+                console.log(await data.execute('https://google.com'));
+            }
+        );
        
     }
 }
