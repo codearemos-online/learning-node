92521c556f07ea36de4c5aaccdceac1953956bb1
codearemos-online <dev@codearemos.online>
2025-06-21 00:40:39 -0500

    feat(json) return a json response

--- classes/class7/src/app.ts
+++ classes/class7/src/app.ts
@@ -4,11 +4,14 @@ import path from 'path';
 
 const server = http.createServer((req,res) => {
     const html = fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf-8')
+    const data = {
+        title: 'Hello World',
+        message: 'This is a message from the server'
+    }
     
     if(req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.write(html)
-        res.end()
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(data))
     } else {
         res.writeHead(404, { 'Content-Type': 'text/html' })
         res.write('Not found')
