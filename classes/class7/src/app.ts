107795ab36b3d8d23027917fbfdb106ce5f71877
codearemos-online <dev@codearemos.online>
2025-06-21 00:24:39 -0500

    feat(http) First call from http to check connection

---
+++ classes/class7/src/app.ts
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const server = http.createServer((req,res) => {
+    console.log(req.url)
+    res.write('Hello World')
+    res.end()
+})
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
