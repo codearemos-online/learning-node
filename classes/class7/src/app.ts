f4d53799ae7680e272c0306c9f9f2dd7b1ec90a0
codearemos-online <dev@codearemos.online>
2025-06-21 00:38:59 -0500

    feat(filesystem html) Reading a filesystem and use like a response

--- classes/class7/src/app.ts
+++ classes/class7/src/app.ts
@@ -1,9 +1,19 @@
 import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
 const server = http.createServer((req,res) => {
-    console.log(req.url)
-    res.write('Hello World')
-    res.end()
+    const html = fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf-8')
+    
+    if(req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(html)
+        res.end()
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        res.write('Not found')
+        res.end()
+    }
 })
 
 server.listen(3000, () => {
