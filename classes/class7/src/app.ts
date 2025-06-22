9a44486be6e52795dd9e7be3fed7537885865b20
codearemos-online <dev@codearemos.online>
2025-06-22 08:05:33 -0500

    feat(webserver) Read external content css and js

--- classes/class7/src/app.ts
+++ classes/class7/src/app.ts
@@ -3,20 +3,30 @@ import fs from 'fs';
 import path from 'path';
 
 const server = http.createServer((req,res) => {
-    const html = fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf-8')
+    const html = fs.readFileSync(path.join(__dirname , '..', 'public', 'index.html'), 'utf-8')
     const data = {
         title: 'Hello World',
         message: 'This is a message from the server'
     }
+
+    console.log(req.url)
     
     if(req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(data))
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' })
-        res.write('Not found')
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(html)
         res.end()
+
+        return;
     }
+
+    if(req.url?.endsWith('.css')) {
+        res.writeHead(200, { 'Content-Type': 'text/css' })
+    } else if(req.url?.endsWith('.js')) {
+        res.writeHead(200, { 'Content-Type': 'application/javascript' })
+    }
+
+    const responseFile = fs.readFileSync(`./public${req.url}`, 'utf-8');
+    res.end(responseFile)
 })
 
 server.listen(3000, () => {
