6430239e7fe00952db95780d3c019854760e81af
codearemos-online <dev@codearemos.online>
2025-06-27 22:02:51 -0500

    feat(env) Using  params from envs

--- classes/class7/src/presentation/server.ts
+++ classes/class7/src/presentation/server.ts
@@ -1,19 +1,32 @@
 import express from 'express';
 import path from 'path';
 
+interface Options {
+    port: number;
+    publicPath: string;
+}
+
 export class Server {
+
+    private readonly port: number;
+    private readonly publicPath: string;
+    constructor(private  options: Options) {
+        this.port = options.port;
+        this.publicPath = options.publicPath;
+    }
+
     private app = express();
 
     start() {
         this.app.use(express.static('public'))
 
         this.app.get('/*splat', (req, res) => {
-            const indexPath = path.join(__dirname, '..', '..', 'public', 'index.html')
+            const indexPath = path.join(__dirname, '..', '..', this.publicPath, 'index.html')
             res.sendFile(indexPath)
         })
 
 
-        this.app.listen(3000, () => {
+        this.app.listen(this.port, () => {
             console.log('Server started on port 3000');
         });
     }
