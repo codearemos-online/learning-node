40db1124a23428054b347c0afa3d8d02aa8ae3ab
codearemos-online <dev@codearemos.online>
2025-06-28 21:04:35 -0500

    feat(rest) Example from express routes

--- classes/class7/src/presentation/server.ts
+++ classes/class7/src/presentation/server.ts
@@ -10,7 +10,7 @@ export class Server {
 
     private readonly port: number;
     private readonly publicPath: string;
-    constructor(private  options: Options) {
+    constructor(private options: Options) {
         this.port = options.port;
         this.publicPath = options.publicPath;
     }
@@ -19,6 +19,20 @@ export class Server {
 
     start() {
         this.app.use(express.static('public'))
+
+        /** ROUTES */
+        this.app.get('/api/todos', (req, res) => {
+            res.json([
+                {
+                    'id': 1,
+                    'task': 'buy milk'
+                },
+                {
+                    'id': 2,
+                    'task': 'buy bread'
+                },
+            ])
+        })
 
         this.app.get('/*splat', (req, res) => {
             const indexPath = path.join(__dirname, '..', '..', this.publicPath, 'index.html')
