38a4238ca468f9feec7de9a109a10173ee953cb3
codearemos-online <dev@codearemos.online>
2025-07-14 21:56:13 -0500

    feat(auth) implement register method and annd DI from authService

---
+++ classes/class8/src/domain/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { UserModel } from "../../data";
+import { RegisterUserDto } from "../dtos/auth/register-user.dto";
+import { CustomError } from "../errors/custom.error";
+
+export class AuthService{
+    constructor(){}
+    
+    async registerUser(userDto:RegisterUserDto){
+        
+        const user = await UserModel.findOne({email:userDto.email}) 
+
+        if(user) throw CustomError.badRequest("email already exists")
+
+        return userDto;
+        
+    }
+}
