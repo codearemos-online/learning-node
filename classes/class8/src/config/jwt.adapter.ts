86205ea34392c0a60becc63f0009dfb6661e54dc
codearemos-online <dev@codearemos.online>
2025-07-24 23:39:22 -0500

    feat(jwt) Config jwt to implement tokens

---
+++ classes/class8/src/config/jwt.adapter.ts
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+import { envs } from "./envs";
+
+const JWT_SECRET = envs.JWT_SECRET;
+
+export class JwtAdapter{    
+    static async generateToken(payload: any, duration: string="2h"){
+        try {
+            return jwt.sign(payload, JWT_SECRET, {expiresIn: duration});
+        } catch (error) {
+            throw error;
+        }
+    }
+}   
